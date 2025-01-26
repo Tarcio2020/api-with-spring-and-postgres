3f2ffa6731d5e0312086c53814ad63afbe96c9ea
Tarcio2020 <tarcioapps7@gmail.com>
2025-01-26 09:57:04 -0300

    project created

---
+++ salesSystem/src/main/java/com/tarcio2020/salesSystem/SalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tarcio2020.salesSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSystemApplication.class, args);
+	}
+
+}
