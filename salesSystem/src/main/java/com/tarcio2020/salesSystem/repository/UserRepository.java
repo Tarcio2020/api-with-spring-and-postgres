b312b5d274049b9b134e3c40d710a3898583da0a
Tarcio2020 <tarcioapps7@gmail.com>
2025-01-27 08:44:44 -0300

    JPA repository, dependency injection, database seeding

---
+++ salesSystem/src/main/java/com/tarcio2020/salesSystem/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tarcio2020.salesSystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tarcio2020.salesSystem.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
