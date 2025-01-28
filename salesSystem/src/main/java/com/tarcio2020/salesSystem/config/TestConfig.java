831bd0e88bd50bb1c3761624a0f5ea82665cfa6e
Tarcio2020 <tarcioapps7@gmail.com>
2025-01-28 17:53:09 -0300

    OrderStatus 'package enums'

--- salesSystem/src/main/java/com/tarcio2020/salesSystem/config/TestConfig.java
+++ salesSystem/src/main/java/com/tarcio2020/salesSystem/config/TestConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.tarcio2020.salesSystem.entities.User;
+import com.tarcio2020.salesSystem.entities.enums.OrderStatus;
 import com.tarcio2020.salesSystem.entities.Order;
 import com.tarcio2020.salesSystem.repository.OrderRepository;
 import com.tarcio2020.salesSystem.repository.UserRepository;
@@ -33,9 +34,9 @@ public class TestConfig implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(u1, u2));
         
         // Criando ordens
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.SHIPPED, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PROCESSING ,u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELLED, u1);
         
         // Salvando ordens
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
